546de0d20344fb7649ee67426ef8ed19351fb960
ashukla2 <ashukla2@IN-L2032.corp.yodlee.com>
2018-12-31 16:12:38 +0530

    20. Implementing update and delete

--- messenger/src/main/java/javabrains/restapi/messenger/resources/MessageResource.java
+++ messenger/src/main/java/javabrains/restapi/messenger/resources/MessageResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,10 +18,6 @@ import javabrains.restapi.messenger.service.MessageService;
 public class MessageResource {
 	MessageService messageService= new MessageService();
 	@GET
-	/*
-	 * Directly we cannot convert to json. In pom.xml, one dependency has been commented out by name "jersey-media-moxy".
-	 * Uncomment it to get json response.
-	 */
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages()
 	{
@@ -35,12 +32,19 @@ public class MessageResource {
 		return messageService.addMessage(message);
 	}
 	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message)
+	{
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
 	@GET
 	@Path("/{messageId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	/*
-	 * 
-	 */
 	public Message test(@PathParam("messageId") long id)
 	{
 		return messageService.getMessage(id);
