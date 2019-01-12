d312c93c788f7c408b67110497c035ff420e89ec
ashukla2 <ashukla2@IN-L2032.corp.yodlee.com>
2019-01-12 10:34:16 +0530

    24. Using Context and BeanParam annotations

--- messenger/src/main/java/javabrains/restapi/messenger/resources/InjectDemoResource.java
+++ messenger/src/main/java/javabrains/restapi/messenger/resources/InjectDemoResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectDemo")
 // For text plain, ensure that content type in postman is empty.
@@ -25,9 +28,27 @@ public class InjectDemoResource
 			                               @HeaderParam("header") String header,
 			                               @CookieParam("cookieName") String cookie)
 	{
-		// header param can be used to get metadata such as authentication token etc.
-		// @FormParam is also there which can be used to capture data entered through html forms
 		return "MatrixParam " + matrixParam + " HeaderParam " + header + " CookieParam " + cookie;
+	}
+	
+	/*
+	 * But there is problem. It can be a trouble to remember the name of all the params.
+	 * For this we have context annotation.
+	 * This annotation gets applied with only certain classes only.
+	 */
+	
+	@GET
+	@Path("context")
+	public String getParamsUsingContext(@Context UriInfo uriinfo,
+			                            @Context HttpHeaders headers)
+	{
+//		String path=uriinfo.getPath();
+//		return "Path is: "+ path;
+//		Output will be 'Path is: injectDemo/context'
+		String path =uriinfo.getAbsolutePath().toString();
+		String cookies=headers.getCookies().toString();
+		return "Absolute path is: "+ path+ " and Cookie is: "+cookies;
+		// Absolute path is: http://localhost:8080/messenger/webapi/injectDemo/context and header is: {hello cookies=$Version=0;hello cookies=}
 	}
 
 }
